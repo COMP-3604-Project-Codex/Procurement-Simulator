d320047252b9d2a7facd9475605ce91b4c4d8d57
Andrews3002 <alexangeloandrews@gmail.com>
2026-03-26 09:54:14 -0400

    rfp class created

---
+++ App/models/RFP.py
@@ -0,0 +1,20 @@
+from App.database import db
+
+class RFP(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    groupID = db.Column(db.Integer, db.ForeignKey('group.id'), nullable=False)
+    lotID = db.Column(db.Integer, db.ForeignKey('lot.id'), nullable=False)
+    specs = db.Column(db.JSON)
+
+    def __init__(self, groupID, lotID, specs):
+        self.groupID = groupID
+        self.lotID = lotID
+        self.specs = specs
+
+    def get_json(self):
+        return {
+            'id': self.id,
+            'groupID': self.groupID,
+            'lotID': self.lotID,
+            'specs': self.specs
+        }
