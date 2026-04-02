cf156223173e2ffeb5e52606990cb7661d46b65f
Andrews3002 <alexangeloandrews@gmail.com>
2026-04-02 16:12:43 -0400

    models final update

--- App/models/group.py
+++ App/models/group.py
@@ -3,15 +3,17 @@ from App.database import db
 class Group(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     groupName = db.Column(db.String(30), nullable=False, unique=True)
+    status = db.Column(db.String(30), nullable=False, default="requested")
 
     def __init__(self, groupName):
         self.groupName = groupName
 
     def set_generated_name(self):
-        self.groupName = f"Group{self.id} {self.groupName}"
+        self.groupName = f"G{self.id} {self.groupName}"
 
     def get_json(self):
         return {
             'id': self.id,
-            'groupName': self.groupName
+            'groupName': self.groupName,
+            'status': self.status
         }
--- App/tests/__init__.py
+++
@@ -1,2 +0,0 @@
-from .A_unit_test import *
-from .B_integration_test import *
