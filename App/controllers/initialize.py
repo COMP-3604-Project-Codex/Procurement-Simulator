68590d6944650f9c07f20b6e3119c024d8a119f3
Andrews3002 <alexangeloandrews@gmail.com>
2026-04-07 10:44:58 -0400

    more professional sample data

--- App/controllers/initialize.py
+++ App/controllers/initialize.py
@@ -29,39 +29,70 @@ def initialize():
     edit_lotRFP_details(6, deviceType="High-End Desktop", cpu="Intel Core i9-13900K", ram="64GB DDR5", resolution="Dual 32-inch 4K OLED", os="Windows 11 Pro", drive="2TB NVMe SSD + 2TB HDD", gpu="NVIDIA RTX 4090", peripherals="4x USB-A, 4x USB-C, 2x Thunderbolt_4, SD Card Reader", features="Wireless Ergonomic Keyboard, Precision Mouse, Drawing Tablet (Wacom)", io="Pantone-Calibrated Display, WiFi_6E, Bluetooth_5.2, Hardware Color Calibrator")
 
 
-    create_student("jack", "20240123", "jackpass")
-    create_student("cooper", "20231245", "cooperpass")
-    create_student("john", "20229876", "johnpass")
-    create_student("ray", "20246789", "raypass")
+    create_student("Aiden Joseph", "20240001", "aidenpass")
+    create_student("Kareem Ali", "20240002", "kareempass")
+    create_student("Joshua Peters", "20240003", "joshuapass")
+    create_student("Daniel Roberts", "20240004", "danielpass")
 
-    create_student("tony", "20235678", "tonypass")
-    create_student("steve", "20242345", "stevepass")
-    create_student("clint", "20238901", "clintpass")
-    create_student("bruce", "20238902", "brucepass")
+    create_student("Samantha Clarke", "20240005", "samanthapass")
+    create_student("Aaliyah Mohammed", "20240006", "aaliyahpass")
+    create_student("Leah Charles", "20240007", "leahpass")
+    create_student("Naomi Thomas", "20240008", "naomipass")
 
-    create_student("ultron", "20264789", "ultronpass")
-    create_student("thanos", "20242435", "thanospass")
-    create_student("loki", "20328901", "lokipass")
-    create_student("red skull", "20235342", "redskullpass")
+    create_student("Ryan Williams", "20240009", "ryanpass")
+    create_student("Darius Edwards", "20240010", "dariuspass")
+    create_student("Marcus Browne", "20240011", "marcuspass")
+    create_student("Andre Lewis", "20240012", "andrepass")
 
-    group = create_group("Avengers")
-    tony = db.session.scalars(db.select(Student).where(Student.username == "20235678")).first()
-    steve = db.session.scalars(db.select(Student).where(Student.username == "20242345")).first()
-    clint = db.session.scalars(db.select(Student).where(Student.username == "20238901")).first()
-    bruce = db.session.scalars(db.select(Student).where(Student.username == "20238902")).first()   
-    members = [tony.id, steve.id, clint.id, bruce.id]
+    create_student("Keisha Grant", "20240013", "keishapass")
+    create_student("Tricia Ramnarine", "20240014", "triciapass")
+    create_student("Anil Singh", "20240015", "anilpass")
+    create_student("Ravi Persad", "20240016", "ravipass")
 
-    for member in members:
-        add_studentGroup(member, group.id)
+    create_student("Janelle Baptiste", "20240017", "janellepass")
+    create_student("Shawn Mitchell", "20240018", "shawnpass")
+    create_student("Kevin Hernandez", "20240019", "kevinpass")
+    create_student("Ashley Gomez", "20240020", "ashleypass")
 
-    ultron = db.session.scalars(db.select(Student).where(Student.username == "20264789")).first()
-    thanos = db.session.scalars(db.select(Student).where(Student.username == "20242435")).first()
-    loki = db.session.scalars(db.select(Student).where(Student.username == "20328901")).first()
-    red_skull = db.session.scalars(db.select(Student).where(Student.username == "20235342")).first()
-    group1 = create_group("Villains")
-    members1 = [ultron.id, thanos.id, loki.id, red_skull.id]
+    # Group 1
+    group1 = create_group("NexoraTech")
+    s1 = db.session.scalars(db.select(Student).where(Student.username == "20240001")).first()
+    s2 = db.session.scalars(db.select(Student).where(Student.username == "20240002")).first()
+    s3 = db.session.scalars(db.select(Student).where(Student.username == "20240003")).first()
 
+    members1 = [s1.id, s2.id, s3.id]
     for member in members1:
         add_studentGroup(member, group1.id)
 
-    
+
+    # Group 2
+    group2 = create_group("QuantumSoft")
+    s4 = db.session.scalars(db.select(Student).where(Student.username == "20240004")).first()
+    s5 = db.session.scalars(db.select(Student).where(Student.username == "20240005")).first()
+    s6 = db.session.scalars(db.select(Student).where(Student.username == "20240006")).first()
+
+    members2 = [s4.id, s5.id, s6.id]
+    for member in members2:
+        add_studentGroup(member, group2.id)
+
+
+    # Group 3
+    group3 = create_group("ByteForge")
+    s7 = db.session.scalars(db.select(Student).where(Student.username == "20240007")).first()
+    s8 = db.session.scalars(db.select(Student).where(Student.username == "20240008")).first()
+    s9 = db.session.scalars(db.select(Student).where(Student.username == "20240009")).first()
+
+    members3 = [s7.id, s8.id, s9.id]
+    for member in members3:
+        add_studentGroup(member, group3.id)
+
+
+    # Group 4
+    group4 = create_group("CyberNova")
+    s10 = db.session.scalars(db.select(Student).where(Student.username == "20240010")).first()
+    s11 = db.session.scalars(db.select(Student).where(Student.username == "20240011")).first()
+    s12 = db.session.scalars(db.select(Student).where(Student.username == "20240012")).first()
+
+    members4 = [s10.id, s11.id, s12.id]
+    for member in members4:
+        add_studentGroup(member, group4.id)
